0a5ac0b0beb946f7e2209743bb711ec16cc952ee
Michael Gissing <michael.gissing@gmx.at>
2011-04-20 14:36:49 +0200

    added license header

--- src/main/java/org/openengsb/connector/maven/internal/MavenConnectorProvider.java
+++ src/main/java/org/openengsb/connector/maven/internal/MavenConnectorProvider.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.openengsb.connector.maven.internal;
 
 import org.openengsb.core.api.descriptor.ServiceDescriptor;
