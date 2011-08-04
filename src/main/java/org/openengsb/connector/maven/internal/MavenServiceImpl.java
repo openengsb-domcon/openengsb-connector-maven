8a76d9945b271df3f1cda77bd156cd77f7685b98
Andreas Pieber <anpieber@gmail.com>
2011-08-04 18:49:41 +0200

    [OPENENGSB-1882] Use maven central to download maven binraries
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- src/main/java/org/openengsb/connector/maven/internal/MavenServiceImpl.java
+++ src/main/java/org/openengsb/connector/maven/internal/MavenServiceImpl.java
@@ -29,7 +29,6 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -113,23 +112,24 @@ public class MavenServiceImpl extends AbstractOpenEngSBService implements BuildD
         }
     }
 
-    private List<String> getListOfMirrors() throws IOException {
-        Properties prop = new Properties();
-        List<String> mirrorList = new ArrayList<String>();
-
-        prop.load(this.getClass().getClassLoader().getResourceAsStream("config.properties"));
-
-        int i = 1;
-        while (prop.getProperty("mirror" + i) != null) {
-            mirrorList.add(prop.getProperty("mirror" + i));
-            i++;
-        }
-
-        return mirrorList;
-    }
-
     public Boolean isMavenInstalled() {
         return new File(System.getProperty("karaf.data"), "apache-maven-" + mvnVersion).exists();
+    }
+
+
+    public void installMaven() throws IOException {
+        File tmp = File.createTempFile("mvn_setup", "zip");
+        initializeDownload(tmp);
+        unzipFile(tmp.getAbsolutePath(), System.getProperty("karaf.data"));
+    }
+
+    private void initializeDownload(File tmp) {
+        String baseUrl = "http://repo1.maven.org/maven2/org/apache/maven/apache-maven";
+        String fullUrl = String.format("%s/%s/apache-maven-%s-bin.zip", baseUrl, mvnVersion, mvnVersion);
+        if (!download(fullUrl, tmp)) {
+            throw new IllegalStateException(
+                String.format("Maven download not possible, because %s does not answer", fullUrl));
+        }
     }
 
     public boolean download(String urlString, File tmp) {
@@ -141,32 +141,6 @@ public class MavenServiceImpl extends AbstractOpenEngSBService implements BuildD
             return false;
         }
         return true;
-    }
-
-    public void installMaven() throws IOException {
-        File tmp = File.createTempFile("mvn_setup", "zip");
-        List<String> mirrors = getListOfMirrors();
-
-        if (mirrors.isEmpty()) {
-            throw new IllegalStateException(
-                    "Maven download not possible, because there are no mirrors specified");
-        } else if (!downloadFromAnyMirror(tmp, mirrors)) {
-            throw new IllegalStateException(
-                    "Maven download not possible, because there are no available mirrors");
-        }
-
-        unzipFile(tmp.getAbsolutePath(), System.getProperty("karaf.data"));
-        new File(mvnCommand).setExecutable(true);
-    }
-
-    private boolean downloadFromAnyMirror(File tmp, List<String> mirrors) {
-        for (String mirrorBaseUrl : mirrors) {
-            String fullUrl = String.format("%sapache-maven-%s-bin.zip", mirrorBaseUrl, mvnVersion);
-            if (download(fullUrl, tmp)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public void unzipFile(String archivePath, String targetPath)
